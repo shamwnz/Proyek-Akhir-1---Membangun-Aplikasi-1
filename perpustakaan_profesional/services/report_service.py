146a149de5e915ac3b6dc8ccd4e08ea95a4d3388
chelsia viontika manalu <12550125531@students.uin-suska.ac.id>
2026-06-05 09:14:31 +0700

    Add total_members method to ReportService

--- perpustakaan_profesional/services/report_service.py
+++ perpustakaan_profesional/services/report_service.py
@@ -1,4 +1,9 @@
 class ReportService:
+
     @staticmethod
     def total_items(items):
-        print(f"\nTotal koleksi perpustakaan: {len(items)} item")
+        print(f"Total Koleksi : {len(items)}")
+
+    @staticmethod
+    def total_members(members):
+        print(f"Total Member : {len(members)}")
